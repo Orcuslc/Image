492b6257ef5240c44a3964fdf6550484e239a777
Orcuslc <orcuslc@hotmail.com>
2017-01-05 22:45:40 +0800

    updated laplacian

--- Filters/sharpening.py
+++ Filters/sharpening.py
@@ -5,17 +5,24 @@ from numpy import *
 
 def laplacian(img, order = 3):
 	temp = zeros((3, 3))
-	temp[0, 1] = temp[1, 0] = temp[1, 2] = temp[2, 1] = -1
-	temp[1, 1] = -4
+	temp[:, :] = 1
+	temp[1, 1] = -8
 	if order == 3:
 		temp = asarray([[[x, x, x] for x in i] for i in temp])
 	nimg = img - conv(img, temp)
-	return nimg
+	return nimg - nimg.min()
 
-
+# def laplacian1(img, order = 3):
+# 	temp = zeros((3, 3))
+# 	temp[0, 1] = temp[1, :] = temp[2, 1] = 1
+# 	temp[1, 1] = -4
+# 	if order == 3:
+# 		temp = asarray([[[x, x, x] for x in i] for i in temp])
+# 	nimg = img - conv(img, temp)
+# 	return nimg
 
 if __name__ == '__main__':
-	img = cv2.imread('../test-images/lion.jpg', 0)	
+	img = cv2.imread('../test-images/test_blurred.jpg', 0)	
 	nimg = laplacian(img, 1)
 	cv2.imshow('img', img)
 	cv2.imshow('nimg', nimg)
