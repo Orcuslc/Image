d61a5d2d2e53ac7711459a0c290e550138943bb1
Orcuslc <orcuslc@hotmail.com>
2017-01-05 22:21:44 +0800

    add test

--- Filters/sharpening.py
+++ Filters/sharpening.py
@@ -3,9 +3,22 @@ from scipy.ndimage import convolve as conv
 import cv2
 from numpy import *
 
-def laplacian():
+def laplacian(img, order = 3):
 	temp = zeros((3, 3))
 	temp[0, 1] = temp[1, 0] = temp[1, 2] = temp[2, 1] = -1
 	temp[1, 1] = -4
-	return temp
+	if order == 3:
+		temp = asarray([[[x, x, x] for x in i] for i in temp])
+	nimg = img - conv(img, temp)
+	return nimg
 
+
+
+if __name__ == '__main__':
+	img = cv2.imread('../test-images/lion.jpg', 0)	
+	nimg = laplacian(img, 1)
+	cv2.imshow('img', img)
+	cv2.imshow('nimg', nimg)
+	k = cv2.waitKey(0)
+	if k == 27:
+		cv2.destroyAllWindows()
--- Pyramid-Based Shadow Removal/detect.py
+++ Pyramid-Based Shadow Removal/detect.py
@@ -5,18 +5,24 @@ cos = np.cos
 norm = np.linalg.norm
 pi = np.pi
 exp = np.exp
+floor = np.floor
 
 def dist(v1, v2):
 	# Compute the illumination invariant color distance (The cosine similarity)
 	return 1 - abs(cos(np.dot(v1, v2)/(norm(v1, ord=2)*norm(v2, ord=2))))
 
-def gaussian_average(img, shape = (5, 5), sigma = 1):
+def gaussian_operator(shape = (5, 5), sigma = 1):
 	# Gaussian Average Operator
-	gaussian_template = np.zeros(shape)
+	template = np.zeros(shape)
+	cx = floor(shape[0]/2)
+	cy = floor(shape[1]/2)
 	for i in range(shape[0]):
 		for j in range(shape[1]):
-			gaussian_template[i, j] = 1/(2*pi*sigma**2)*exp(-((i+1)**2+(j+1)**2)/(2*sigma**2))
-	return conv(img, gaussian_template)
+			template[i, j] = 1/(2*pi*sigma**2)*exp(-((i+1-cx)**2+(j+1-cy)**2)/(2*sigma**2))
+	return template
+
+def sobel_operator(size):
+	
 
 def canny_border(img):
 	# Detect Borders with Canny Border Detection
